674d92f2ecc9c3f2cd16cad0c371ab092b9d6935
Gustavo Fernandes Campos <gustavo.campos@sp01.local>
2019-11-12 13:32:49 -0300

    Class e Style binding -  Aula

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  // AULA 01
+  // url: string = 'http://loiane.com.br';
+  // cursoAngular: boolean = true;
+  // tslint:disable-next-line: max-line-length
+  // urlImagem = "https://www.researchgate.net/profile/Marcelo_Dantas3/publication/305045451/figure/fig6/AS:381662237085700@1468006776450/Figura-36-Planicie-fluvial-meandrante-do-rio-Guapore-na-fronteira-Brasil-Bolivia.png"
+  // getValor(){
+  //  return 1;
+  // }
+  // getCurtirCurso(){
+  //  return true;
+  // }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
